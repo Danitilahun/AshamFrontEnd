481c20749dd40243851a81b30f070018b2cec89e
Daniel Tilahun <tiledan2015@gmail.com>
2023-10-16 21:24:39 +0300

    adding

--- src/components/Asbeza/EditForm/callcenterForm.jsx
+++ src/components/Asbeza/EditForm/callcenterForm.jsx
@@ -148,6 +148,18 @@ const EditAsbezaOrderForm = ({
           };
         }
 
+        if (!values.callcenterId) {
+          handleCloseForm();
+          throw {
+            response: {
+              data: {
+                message:
+                  "Call center information is not found. Please check your connection, refresh your browser, and try again.",
+                type: "error",
+              },
+            },
+          };
+        }
         values.status = "new order";
         values.blockHouse = values.blockHouse.toUpperCase();
         console.log("values", values);
