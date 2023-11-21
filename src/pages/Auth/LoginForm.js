65ae33c207d12d67d9c01c6539d3ec6eb920823a
Daniel Tilahun <tiledan2015@gmail.com>
2023-11-22 00:04:30 +0300

    adding login page

--- src/pages/Auth/LoginForm.js
+++ src/pages/Auth/LoginForm.js
@@ -188,8 +188,6 @@ import {
   Button,
   CircularProgress,
   Container,
-  Dialog,
-  DialogContent,
   IconButton,
   InputAdornment,
   Paper,
@@ -207,7 +205,7 @@ import { useAuth } from "../../contexts/AuthContext";
 import ErrorAlert from "../../components/VersatileComponents/ErrorAlert";
 import { Helmet } from "react-helmet";
 
-const bgImage = "/assets/bg.jpg";
+const bgImage = "/assets/login.png";
 const initialValues = {
   email: "",
   password: "",
@@ -220,25 +218,12 @@ const validationSchema = Yup.object().shape({
     .required("Required"),
 });
 
-const Transition = React.forwardRef(function Transition(props, ref) {
-  return <Slide direction="up" ref={ref} {...props} />;
-});
-
 const LoginForm = () => {
   const [showPassword, setShowPassword] = useState(false);
   const { login, isLoading } = useAuth();
   const [error, setError] = useState(null);
   const navigate = useNavigate();
   const theme = useTheme();
-  const [openDialog, setOpenDialog] = useState(false);
-
-  const handleOpenDialog = () => {
-    setOpenDialog(true);
-  };
-
-  const handleCloseDialog = () => {
-    setOpenDialog(false);
-  };
 
   const handlePasswordVisibility = () => {
     setShowPassword((prev) => !prev);
@@ -257,27 +242,9 @@ const LoginForm = () => {
         <title>Login Page </title>
         <meta name="description" content="login page" />
       </Helmet>
-      <AppBar
-        position="sticky"
-        // style={{ background: theme.palette.primary.main }}
-      >
-        <Toolbar>
-          <Typography
-            variant="h3"
-            component="div"
-            sx={{ flexGrow: 1, color: theme.palette.text.secondary }}
-          >
-            AshamLole
-          </Typography>
-          <Button color="inherit" onClick={handleOpenDialog}>
-            Login
-          </Button>
-        </Toolbar>
-      </AppBar>
 
       <Container
         component="main"
-        // maxWidth="xs"
         style={{
           display: "flex",
           justifyContent: "center",
@@ -285,124 +252,107 @@ const LoginForm = () => {
           height: "100vh",
         }}
       >
-        <Dialog
-          open={openDialog}
-          onClose={handleCloseDialog}
-          TransitionComponent={Transition}
-        >
-          <DialogContent style={{ width: "400px" }}>
-            {/* <Paper
-              elevation={3}
-              style={{
-                padding: 16,
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-          
-              }}
-            > */}
-            <Typography component="h1" variant="h5">
-              Login
-            </Typography>
-            {error && <ErrorAlert message={error} />}
-            <Formik
-              initialValues={initialValues}
-              validationSchema={validationSchema}
-              onSubmit={(values) => {
-                login(values.email, values.password)
-                  .then((res) => {
-                    navigate("/log");
-                  })
-                  .catch((error) => {
-                    setError(error.code);
-                  });
-              }}
-            >
-              {({ errors, touched }) => (
-                <Form style={{ width: "100%", marginTop: 8 }}>
-                  <Field
-                    as={TextField}
-                    variant="outlined"
-                    margin="normal"
-                    fullWidth
-                    id="email"
-                    label="Email Address"
-                    name="email"
-                    autoComplete="email"
-                    helperText={touched.email ? errors.email : ""}
-                    error={touched.email && Boolean(errors.email)}
-                  />
+        <Paper elevation={3} style={{ padding: 16, width: 400 }}>
+          <Typography component="h1" variant="h5" align="center">
+            Login
+          </Typography>
+          {error && <ErrorAlert message={error} />}
+          <Formik
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={(values) => {
+              login(values.email, values.password)
+                .then((res) => {
+                  navigate("/log");
+                })
+                .catch((error) => {
+                  setError(error.code);
+                });
+            }}
+          >
+            {({ errors, touched }) => (
+              <Form style={{ width: "100%", marginTop: 8 }}>
+                <Field
+                  as={TextField}
+                  variant="outlined"
+                  margin="normal"
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                  helperText={touched.email ? errors.email : ""}
+                  error={touched.email && Boolean(errors.email)}
+                />
 
-                  <Field
-                    as={TextField}
-                    variant="outlined"
-                    margin="normal"
-                    fullWidth
-                    id="password"
-                    label="Password"
-                    name="password"
-                    type={showPassword ? "text" : "password"}
-                    autoComplete="new-password"
-                    helperText={touched.password ? errors.password : ""}
-                    error={touched.password && Boolean(errors.password)}
-                    InputProps={{
-                      endAdornment: (
-                        <InputAdornment position="end">
-                          <IconButton
-                            edge="end"
-                            aria-label="toggle password visibility"
-                            onClick={handlePasswordVisibility}
-                            onMouseDown={(e) => e.preventDefault()}
-                          >
-                            {showPassword ? <Visibility /> : <VisibilityOff />}
-                          </IconButton>
-                        </InputAdornment>
-                      ),
-                    }}
-                  />
+                <Field
+                  as={TextField}
+                  variant="outlined"
+                  margin="normal"
+                  fullWidth
+                  id="password"
+                  label="Password"
+                  name="password"
+                  type={showPassword ? "text" : "password"}
+                  autoComplete="new-password"
+                  helperText={touched.password ? errors.password : ""}
+                  error={touched.password && Boolean(errors.password)}
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position="end">
+                        <IconButton
+                          edge="end"
+                          aria-label="toggle password visibility"
+                          onClick={handlePasswordVisibility}
+                          onMouseDown={(e) => e.preventDefault()}
+                        >
+                          {showPassword ? <Visibility /> : <VisibilityOff />}
+                        </IconButton>
+                      </InputAdornment>
+                    ),
+                  }}
+                />
 
-                  <Typography
-                    variant="body2"
-                    sx={{
-                      textAlign: "end",
-                      padding: "10px",
-                      fontSize: "1rem",
+                <Typography
+                  variant="body2"
+                  sx={{
+                    textAlign: "end",
+                    padding: "10px",
+                    fontSize: "1rem",
+                    color: theme.palette.text.secondary,
+                    fontWeight: 500,
+                  }}
+                >
+                  <Link
+                    to="/forget-password"
+                    color="secondary"
+                    style={{
+                      textDecoration: "none",
                       color: theme.palette.text.secondary,
-                      fontWeight: 500,
                     }}
                   >
-                    <Link
-                      to="/forget-password"
-                      color="secondary"
-                      style={{
-                        textDecoration: "none",
-                        color: theme.palette.text.secondary,
-                      }}
-                    >
-                      Forgot Password
-                    </Link>
-                  </Typography>
+                    Forgot Password
+                  </Link>
+                </Typography>
 
-                  <Button
-                    type="submit"
-                    fullWidth
-                    variant="contained"
-                    color="primary"
-                    style={{ margin: "24px 0 16px" }}
-                    disabled={isLoading}
-                  >
-                    {isLoading ? (
-                      <CircularProgress size={24} color="inherit" />
-                    ) : (
-                      "Sign In"
-                    )}
-                  </Button>
-                </Form>
-              )}
-            </Formik>
-            {/* </Paper> */}
-          </DialogContent>
-        </Dialog>
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  color="primary"
+                  style={{ margin: "24px 0 16px" }}
+                  disabled={isLoading}
+                >
+                  {isLoading ? (
+                    <CircularProgress size={24} color="inherit" />
+                  ) : (
+                    "Sign In"
+                  )}
+                </Button>
+              </Form>
+            )}
+          </Formik>
+        </Paper>
       </Container>
     </Box>
   );
